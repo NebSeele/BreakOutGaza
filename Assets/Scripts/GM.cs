038be5fa9d150a759395a97dcfc3a1d9696a7c7a
NebSeele <SeeleSPh@hotmail.com>
2017-11-02 14:43:10 +1000

    Moved Project to Repository

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GM : MonoBehaviour {
+
+    public int lives = 3;
+    public int bricks = 54;
+    public float resetDelay = 1f;
+    public Text livesText;
+    public GameObject gameOver;
+    public GameObject levelClear;
+    public GameObject Sandbricks;
+    public GameObject GoldBricks;
+    public GameObject paddle;
+    //public GameObject deathParticles;
+    public static GM instance = null;
+
+    private GameObject clonePaddle;
+
+	// Use this for initialization
+	void Start () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        Setup();
+    }
+
+    public void Setup()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+        Instantiate(Sandbricks, transform.position, Quaternion.identity);
+    }
+
+    void CheckGameOver()
+    {
+        if (bricks < 1)
+        {
+            levelClear.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke ("Reset, resetDelay");
+        }
+        if (lives < 1)
+        {
+            gameOver.SetActive(true);
+            Time.timeScale = .25f;
+            Invoke("Reset, resetDelay");
+        }
+
+    }
+
+    private void Invoke(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void loseLife()
+    {
+        lives--;
+        livesText.text = "Lives:" + lives;
+        Destroy(clonePaddle);
+        Invoke("SetupPaddle", resetDelay);
+        CheckGameOver();
+    }
+
+    void SetupPaddle()
+    {
+        clonePaddle = Instantiate(paddle, transform.position, Quaternion.identity) as GameObject;
+    }
+
+    public void DestroyBrick()
+    {
+        bricks--;
+        CheckGameOver();
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+	
+	public float paddleSpeed = 1f;
+
+	private Vector3 playerPos = new Vector3 (0, -9.5f, 0);
+	
+	// Update is called once per frame
+	void Update () {
+		float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -8f, 8f), -9.5f, 0f);
+        transform.position = playerPos;
+	}
+}
+
+/* Mobile version, to implement later when project is in full development
+ * void Update()
+{
+    float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+
+    if (Input.touchCount == 1)
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.position.x > Screen.width / 2)
+        {
+            xPos = transform.position.x + paddleSpeed;
+        }
+        else
+        {
+            xPos = transform.position.x - paddleSpeed;
+        }
+        //xPos = -7.5f + 15 * touch.position.x/Screen.width;
+
+    }
+    playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);
+    transform.position = playerPos;
+}
+*/
