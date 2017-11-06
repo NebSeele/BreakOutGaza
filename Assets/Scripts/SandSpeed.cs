027fdde6239977519058250e3713e547c353c574
NebSeele <SeeleSPh@hotmail.com>
2017-11-07 00:39:23 +1000

    Quicksand Update

---
+++ Assets/Scripts/SandSpeed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandSpeed : MonoBehaviour {
+
+        public float scrollSpeed = 0.5F;
+        public Renderer rend;
+        void Start()
+        {
+            rend = GetComponent<Renderer>();
+        }
+        void Update()
+        {
+            float offset = Time.time * scrollSpeed;
+            rend.material.SetTextureOffset("_QuickSand", new Vector2(offset, 0));
+        }
+    }
