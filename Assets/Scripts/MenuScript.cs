152e5b84cd7d0cb9dad43413cad70c6b81d7b254
NebSeele <SeeleSPh@hotmail.com>
2017-11-15 00:30:07 +1000

    PC Version Wall Pillar Created

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField] private string m_levelName;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartNewGame()
+    {
+        /* Testing purposes
+        Debug.Log("Start New Game"); */
+        SceneManager.LoadScene(m_levelName, LoadSceneMode.Single);
+        
+    }
+
+    public void SelectALevel()
+    {
+
+        Debug.Log("Select Level not implemented");
+    }
+    public void HighScoreDisplay()
+    {
+
+        Debug.Log("High Score display not implemented");
+    }
+}
