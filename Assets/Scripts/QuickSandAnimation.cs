01288c333bf58176a7c67cefa281c030eb978930
NebSeele <SeeleSPh@hotmail.com>
2017-11-30 09:57:19 +1000

    Quicksand updates

--- Assets/Scripts/QuickSandAnimation.cs
+++ Assets/Scripts/QuickSandAnimation.cs
@@ -8,17 +8,16 @@ using UnityEngine;
  * 
  * Until then it will essentially do nothing.
  * 
- * 
+ */
 public class QuickSandAnimation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   [SerializeField] private float ScrollX = 0.5f;
+   [SerializeField] private float ScrollY = 0.5f;
+
+    // Update is called once per frame
+    void Update () {
+		float OffsetX = Time.time * ScrollX;
+        float OffsetY = Time.time * ScrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+    }
 }
-*/
