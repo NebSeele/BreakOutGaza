fae5508e6217fa71d9f8edfb810e5847d9277b87
NebSeele <SeeleSPh@hotmail.com>
2017-12-07 14:14:49 +1000

    UI and settings update

---
+++ Assets/Scripts/SettingsControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsControl : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+    //public AudioMixer audioMixer;
+
+    public void SetMaster (float volumeMaster)
+     {
+        masterMixer.SetFloat("volumeMaster", volumeMaster);
+     }
+/* Consult about sub mixers when an opportunity arises
+    public void SetAudio(float volumeAudio)
+    {
+        masterMixer.SetFloat("volumeAudio", volumeAudio);
+    }
+ */
+
+}
